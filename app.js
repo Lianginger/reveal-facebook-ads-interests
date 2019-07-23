33b1ec55987ea08bd9b431513bed119b20dd5ffd
lianginger <jackyliang444@hotmail.com.tw>
2019-07-23 23:25:43 +0800

    Set axios to get api data

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
-app.get('/', (req, res) => {
-  res.send('hello word!')
+require('dotenv').config()
+app.get('/:keyword', (req, res) => {
+  const keyword = req.params.keyword.toUpperCase()
+  axios
+    .get(
+      `https://graph.facebook.com/search?type=adinterest&q=[${keyword}]&limit=10000&locale=en_US&access_token=${
+        process.env.ACCESS_TOKEN
+      }`
+    )
+    .then(response => {
+      res.json(response.data)
+    })
 })
 
 app.listen(port, () => {
