fffb6cf897ec7fb0449aa5f9e0ab075e7e9e0153
lianginger <jackyliang444@hotmail.com.tw>
2019-07-26 01:09:56 +0800

    Add .env.example and display interest on page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const axios = require('axios')
+const fetch = require('node-fetch')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 
@@ -20,12 +21,22 @@ app.get('/', function(req, res) {
 
   let keyword = req.query.keyword.toUpperCase()
   keyword = keyword.charAt(0).toUpperCase() + keyword.slice(1).toLowerCase()
+  let encodeKeyword = encodeURI(keyword)
 
-  let URL1 = `https://graph.facebook.com/search?type=adinterest&q=[${keyword}]&limit=10000&locale=zh_TW&access_token=${process.env.ACCESS_TOKEN}`
-  let URL2 = `https://graph.facebook.com/search?type=adinterestsuggestion&interest_list=['${keyword}']&limit=1000&locale=en_US&access_token=${process.env.ACCESS_TOKEN}`
+  let URL1 = `https://graph.facebook.com/search?type=adinterest&q=[%27${encodeKeyword}%27]&limit=10000&locale=zh_TW&access_token=${process.env.ACCESS_TOKEN}`
+  console.log(URL1)
 
-  const promise1 = axios.get(URL1)
-  const promise2 = axios.get(URL2)
+  let promise1 = axios.get(URL1)
+
+  let promise2 = new Promise(resolve => {
+    resolve({ data: { name: 'hi' } })
+  })
+
+  // adinterestsuggestion not support chinese character
+  if (!req.query.keyword.match(/[\u3400-\u9FBF]/)) {
+    let URL2 = `https://graph.facebook.com/search?type=adinterestsuggestion&interest_list=['${encodeKeyword}']&limit=1000&locale=en_US&access_token=${process.env.ACCESS_TOKEN}`
+    promise2 = axios.get(URL2)
+  }
 
   Promise.all([promise1, promise2]).then(function(values) {
     console.log(values)
