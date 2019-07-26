ca65f6dba4881b29b430035122702508014eeaa1
Lianginger <jackyliang444@hotmail.com.tw>
2019-07-26 10:55:07 +0800

    Add googl translate to set chinese character in url

--- app.js
+++ app.js
@@ -9,37 +9,42 @@ const axios = require('axios')
 const fetch = require('node-fetch')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const translateToEng = require('./translate')
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', function(req, res) {
+app.get('/', async function(req, res) {
   if (req.query.keyword === undefined) {
     return res.render('home')
   }
 
   let keyword = req.query.keyword.toUpperCase()
   keyword = keyword.charAt(0).toUpperCase() + keyword.slice(1).toLowerCase()
-  let encodeKeyword = encodeURI(keyword)
 
-  let URL1 = `https://graph.facebook.com/search?type=adinterest&q=[%27${encodeKeyword}%27]&limit=10000&locale=zh_TW&access_token=${process.env.ACCESS_TOKEN}`
-  console.log(URL1)
+  let keywordForAdinterest = keyword
+  let keywordForAdinterestsuggestion = keyword
 
-  let promise1 = axios.get(URL1)
+  // Handle chinese character
+  if (req.query.keyword.match(/[\u3400-\u9FBF]/)) {
+    keywordForAdinterest = encodeURI(keyword)
 
-  let promise2 = new Promise(resolve => {
-    resolve({ data: { name: 'hi' } })
-  })
-
-  // adinterestsuggestion not support chinese character
-  if (!req.query.keyword.match(/[\u3400-\u9FBF]/)) {
-    let URL2 = `https://graph.facebook.com/search?type=adinterestsuggestion&interest_list=['${encodeKeyword}']&limit=1000&locale=en_US&access_token=${process.env.ACCESS_TOKEN}`
-    promise2 = axios.get(URL2)
+    // URL2 adinterestsuggestion api not support chinese character
+    // Use google translate api to translate
+    let text = await translateToEng(keyword)
+    keywordForAdinterestsuggestion = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
   }
 
+  let URL1 = `https://graph.facebook.com/search?type=adinterest&q=[%27${keywordForAdinterest}%27]&limit=10000&locale=zh_TW&access_token=${process.env.ACCESS_TOKEN}`
+  let URL2 = `https://graph.facebook.com/search?type=adinterestsuggestion&interest_list=['${keywordForAdinterestsuggestion}']&limit=1000&locale=en_US&access_token=${
+    process.env.ACCESS_TOKEN
+  }`
+
+  let promise1 = axios.get(URL1)
+  let promise2 = axios.get(URL2)
+
   Promise.all([promise1, promise2]).then(function(values) {
-    console.log(values)
     const adinterest = values[0].data
     const adinterestsuggestion = values[1].data
     res.render('home', { keyword, adinterest, adinterestsuggestion })
---
+++ translate.js
@@ -0,0 +1,22 @@
+module.exports = async function translate(
+  textToTranslate,
+  projectId = process.env.GOOGLE_PROJECTID // Your GCP Project Id
+) {
+  // Imports the Google Cloud client library
+  const { Translate } = require('@google-cloud/translate')
+
+  // Instantiates a client
+  const translate = new Translate({ projectId })
+
+  // The text to translate
+  const text = textToTranslate
+
+  // The target language
+  const target = 'en'
+
+  // Translates some text into Russian
+  const [translation] = await translate.translate(text, target)
+  console.log(`Text: ${text}`)
+  console.log(`Translation: ${translation}`)
+  return translation
+}
